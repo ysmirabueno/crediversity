e3b289726fcdce8e2b830bf4654dea22c1cd4da6
ysmirabueno <140433739+ysmirabueno@users.noreply.github.com>
2026-08-12 12:46:53 -0700

    final_updates

---
+++ script.js
@@ -0,0 +1,32 @@
+function revealAnswer() {
+    const answerDiv = document.getElementById("answerContainer");
+    
+    // Toggle the display between 'none' and 'block'
+    if (answerDiv.style.display === "none") {
+        answerDiv.style.display = "block";
+    } else {
+        answerDiv.style.display = "none";
+    }
+}
+
+function revealAnswer2() {
+    const answerDiv = document.getElementById("answerContainer2");
+    
+    // Toggle the display between 'none' and 'block'
+    if (answerDiv.style.display === "none") {
+        answerDiv.style.display = "block";
+    } else {
+        answerDiv.style.display = "none";
+    }
+}
+
+function revealAnswer3() {
+    const answerDiv = document.getElementById("answerContainer3");
+    
+    // Toggle the display between 'none' and 'block'
+    if (answerDiv.style.display === "none") {
+        answerDiv.style.display = "block";
+    } else {
+        answerDiv.style.display = "none";
+    }
+}
